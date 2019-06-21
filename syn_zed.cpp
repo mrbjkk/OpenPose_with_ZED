2cb91f4585f70a4d3f36889e70c31513b9180979
Yurik <zhaoyaodadc@gmail.com>
2019-06-21 22:25:58 +0800

    slightly modified and align the format

--- syn_zed.cpp
+++ syn_zed.cpp
@@ -19,81 +19,81 @@ DEFINE_string(image_dir, "/home/yurik/Pictures/", "test text");
 
 class WUserInput : public op::WorkerProducer<std::shared_ptr<std::vector<std::shared_ptr<op::Datum>>>>
 {
-public:
-    WUserInput(std::vector<uint32_t> cam_ids, bool use3d)
-		: mUse3d(use3d),
-		  mParamReader(std::make_shared<op::CameraParameterReader>())
+    public:
+        WUserInput(std::vector<uint32_t> cam_ids, bool use3d)
+            : mUse3d(use3d),
+            mParamReader(std::make_shared<op::CameraParameterReader>())
     {
-    	for(const auto& cam_id : cam_ids) {
-    		mCams.emplace_back(std::make_shared<op::WebcamReader>( cam_id ));
-    	}
-    	if (use3d) {
-    		mParamReader->readParameters(FLAGS_calibration_dir);
-    		mIntrinsics = mParamReader->getCameraIntrinsics();
-    		mExtrinsics = mParamReader->getCameraExtrinsics();
-    		mMatrices = mParamReader->getCameraMatrices();
-    	}
-    }
-
-    void initializationOnThread() {}
-
-    std::shared_ptr<std::vector<std::shared_ptr<op::Datum>>> workProducer()
-    {
-        try
-        {
-        	std::lock_guard<std::mutex> g(lock);
-        	if(mBlocked.empty()) {
-
-				for (size_t i = 0; i < mCams.size(); i++) {
-					auto datumsPtr = std::make_shared<std::vector<std::shared_ptr<op::Datum>>>();
-					// Create new datum
-					datumsPtr->emplace_back();
-					auto& datum = datumsPtr->back();
-					datum = std::make_shared<op::Datum>();
-
-					// Fill datum
-					datum->cvInputData = mCams[i]->getFrame();
-					datum->cvOutputData = datum->cvInputData;
-					datum->subId = i;
-					datum->subIdMax = mCams.size() - 1;
-					if(mUse3d) {
-						datum->cameraIntrinsics = mIntrinsics[i];
-						datum->cameraExtrinsics = mExtrinsics[i];
-						datum->cameraMatrix = mMatrices[i];
-					}
-
-					// If empty frame -> return nullptr
-					if (datum->cvInputData.empty())
-					{
-						this->stop();
-						return nullptr;
-					}
-
-					mBlocked.push(datumsPtr);
-				}
-        	}
-
-			auto ret = mBlocked.front();
-			mBlocked.pop();
-			return ret;
+        for(const auto& cam_id : cam_ids) {
+            mCams.emplace_back(std::make_shared<op::WebcamReader>( cam_id ));
         }
-        catch (const std::exception& e)
-        {
-            this->stop();
-            op::error(e.what(), __LINE__, __FUNCTION__, __FILE__);
-            return nullptr;
+        if (use3d) {
+            mParamReader->readParameters(FLAGS_calibration_dir);
+            mIntrinsics = mParamReader->getCameraIntrinsics();
+            mExtrinsics = mParamReader->getCameraExtrinsics();
+            mMatrices = mParamReader->getCameraMatrices();
         }
     }
 
-private:
-    bool mUse3d;
-    std::shared_ptr<op::CameraParameterReader> mParamReader;
-    std::vector<cv::Mat> mIntrinsics;
-    std::vector<cv::Mat> mExtrinsics;
-    std::vector<cv::Mat> mMatrices;
-    std::vector<std::shared_ptr<op::WebcamReader>> mCams;
-    std::queue<std::shared_ptr<std::vector<std::shared_ptr<op::Datum>>>> mBlocked;
-    std::mutex lock;
+        void initializationOnThread() {}
+
+        std::shared_ptr<std::vector<std::shared_ptr<op::Datum>>> workProducer()
+        {
+            try
+            {
+                std::lock_guard<std::mutex> g(lock);
+                if(mBlocked.empty()) {
+
+                    for (size_t i = 0; i < mCams.size(); i++) {
+                        auto datumsPtr = std::make_shared<std::vector<std::shared_ptr<op::Datum>>>();
+                        // Create new datum
+                        datumsPtr->emplace_back();
+                        auto& datum = datumsPtr->back();
+                        datum = std::make_shared<op::Datum>();
+
+                        // Fill datum
+                        datum->cvInputData = mCams[i]->getFrame();
+                        datum->cvOutputData = datum->cvInputData;
+                        datum->subId = i;
+                        datum->subIdMax = mCams.size() - 1;
+                        if(mUse3d) {
+                            datum->cameraIntrinsics = mIntrinsics[i];
+                            datum->cameraExtrinsics = mExtrinsics[i];
+                            datum->cameraMatrix = mMatrices[i];
+                        }
+
+                        // If empty frame -> return nullptr
+                        if (datum->cvInputData.empty())
+                        {
+                            this->stop();
+                            return nullptr;
+                        }
+
+                        mBlocked.push(datumsPtr);
+                    }
+                }
+
+                auto ret = mBlocked.front();
+                mBlocked.pop();
+                return ret;
+            }
+            catch (const std::exception& e)
+            {
+                this->stop();
+                op::error(e.what(), __LINE__, __FUNCTION__, __FILE__);
+                return nullptr;
+            }
+        }
+
+    private:
+        bool mUse3d;
+        std::shared_ptr<op::CameraParameterReader> mParamReader;
+        std::vector<cv::Mat> mIntrinsics;
+        std::vector<cv::Mat> mExtrinsics;
+        std::vector<cv::Mat> mMatrices;
+        std::vector<std::shared_ptr<op::WebcamReader>> mCams;
+        std::queue<std::shared_ptr<std::vector<std::shared_ptr<op::Datum>>>> mBlocked;
+        std::mutex lock;
 };
 
 
@@ -105,7 +105,7 @@ void configureWrapper(op::Wrapper& opWrapper)
 
         // logging_level
         op::check(0 <= FLAGS_logging_level && FLAGS_logging_level <= 255, "Wrong logging_level value.",
-                  __LINE__, __FUNCTION__, __FILE__);
+                __LINE__, __FUNCTION__, __FILE__);
         op::ConfigureLog::setPriorityThreshold((op::Priority)FLAGS_logging_level);
         op::Profiler::setDefaultX(FLAGS_profile_speed);
 
@@ -130,7 +130,7 @@ void configureWrapper(op::Wrapper& opWrapper)
         const auto keypointScaleMode = op::flagsToScaleMode(FLAGS_keypoint_scale);
         // heatmaps to add
         const auto heatMapTypes = op::flagsToHeatMaps(FLAGS_heatmaps_add_parts, FLAGS_heatmaps_add_bkg,
-                                                      FLAGS_heatmaps_add_PAFs);
+                FLAGS_heatmaps_add_PAFs);
         const auto heatMapScaleMode = op::flagsToHeatMapScaleMode(FLAGS_heatmaps_scale);
         // >1 camera view?
         // const auto multipleView = (FLAGS_3d || FLAGS_3d_views > 1 || FLAGS_flir_camera);
@@ -151,23 +151,23 @@ void configureWrapper(op::Wrapper& opWrapper)
         // Pose configuration (use WrapperStructPose{} for default and recommended configuration)
         const op::WrapperStructPose wrapperStructPose{
             poseMode, netInputSize, outputSize, keypointScaleMode, FLAGS_num_gpu, FLAGS_num_gpu_start,
-            FLAGS_scale_number, (float)FLAGS_scale_gap, op::flagsToRenderMode(FLAGS_render_pose, multipleView),
-            poseModel, !FLAGS_disable_blending, (float)FLAGS_alpha_pose, (float)FLAGS_alpha_heatmap,
-            FLAGS_part_to_show, FLAGS_model_folder, heatMapTypes, heatMapScaleMode, FLAGS_part_candidates,
-            (float)FLAGS_render_threshold, FLAGS_number_people_max, FLAGS_maximize_positives, FLAGS_fps_max,
-            FLAGS_prototxt_path, FLAGS_caffemodel_path, (float)FLAGS_upsampling_ratio, enableGoogleLogging};
+                FLAGS_scale_number, (float)FLAGS_scale_gap, op::flagsToRenderMode(FLAGS_render_pose, multipleView),
+                poseModel, !FLAGS_disable_blending, (float)FLAGS_alpha_pose, (float)FLAGS_alpha_heatmap,
+                FLAGS_part_to_show, FLAGS_model_folder, heatMapTypes, heatMapScaleMode, FLAGS_part_candidates,
+                (float)FLAGS_render_threshold, FLAGS_number_people_max, FLAGS_maximize_positives, FLAGS_fps_max,
+                FLAGS_prototxt_path, FLAGS_caffemodel_path, (float)FLAGS_upsampling_ratio, enableGoogleLogging};
         opWrapper.configure(wrapperStructPose);
         // Face configuration (use op::WrapperStructFace{} to disable it)
         const op::WrapperStructFace wrapperStructFace{
             FLAGS_face, faceDetector, faceNetInputSize,
-            op::flagsToRenderMode(FLAGS_face_render, multipleView, FLAGS_render_pose),
-            (float)FLAGS_face_alpha_pose, (float)FLAGS_face_alpha_heatmap, (float)FLAGS_face_render_threshold};
+                op::flagsToRenderMode(FLAGS_face_render, multipleView, FLAGS_render_pose),
+                (float)FLAGS_face_alpha_pose, (float)FLAGS_face_alpha_heatmap, (float)FLAGS_face_render_threshold};
         opWrapper.configure(wrapperStructFace);
         // Hand configuration (use op::WrapperStructHand{} to disable it)
         const op::WrapperStructHand wrapperStructHand{
             FLAGS_hand, handDetector, handNetInputSize, FLAGS_hand_scale_number, (float)FLAGS_hand_scale_range,
-            op::flagsToRenderMode(FLAGS_hand_render, multipleView, FLAGS_render_pose), (float)FLAGS_hand_alpha_pose,
-            (float)FLAGS_hand_alpha_heatmap, (float)FLAGS_hand_render_threshold};
+                op::flagsToRenderMode(FLAGS_hand_render, multipleView, FLAGS_render_pose), (float)FLAGS_hand_alpha_pose,
+                (float)FLAGS_hand_alpha_heatmap, (float)FLAGS_hand_render_threshold};
         opWrapper.configure(wrapperStructHand);
         // Extra functionality configuration (use op::WrapperStructExtra{} to disable it)
         const op::WrapperStructExtra wrapperStructExtra{
@@ -176,10 +176,10 @@ void configureWrapper(op::Wrapper& opWrapper)
         // Output (comment or use default argument to disable any output)
         const op::WrapperStructOutput wrapperStructOutput{
             FLAGS_cli_verbose, FLAGS_write_keypoint, op::stringToDataFormat(FLAGS_write_keypoint_format),
-            FLAGS_write_json, FLAGS_write_coco_json, FLAGS_write_coco_json_variants, FLAGS_write_coco_json_variant,
-            FLAGS_write_images, FLAGS_write_images_format, FLAGS_write_video, FLAGS_write_video_fps,
-            FLAGS_write_video_with_audio, FLAGS_write_heatmaps, FLAGS_write_heatmaps_format, FLAGS_write_video_3d,
-            FLAGS_write_video_adam, FLAGS_write_bvh, FLAGS_udp_host, FLAGS_udp_port};
+                FLAGS_write_json, FLAGS_write_coco_json, FLAGS_write_coco_json_variants, FLAGS_write_coco_json_variant,
+                FLAGS_write_images, FLAGS_write_images_format, FLAGS_write_video, FLAGS_write_video_fps,
+                FLAGS_write_video_with_audio, FLAGS_write_heatmaps, FLAGS_write_heatmaps_format, FLAGS_write_video_3d,
+                FLAGS_write_video_adam, FLAGS_write_bvh, FLAGS_udp_host, FLAGS_udp_port};
         opWrapper.configure(wrapperStructOutput);
         // GUI (comment or use default argument to disable any visual output)
         const op::WrapperStructGui wrapperStructGui{
